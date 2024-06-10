a78ac252b83549a115d396f92042feb261b8bae8
Anna Chaplygina <dupreseraphine@gmail.com>
2024-06-10 19:38:57 +0300

    new commit

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,12 +1,37 @@
-<template>
+<script setup>
 
+import HeadDrawer from "@/components/HeadDrawer.vue";
+import CartItemList from "@/components/CartItemList.vue";
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+    <HeadDrawer/>
+    <CartItemList/>
+
+    <div class="flex flex-col gap-4 mt-7">
+
+      <div class="flex gap-2">
+        <span>Итого:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>22300 ₽</b>
+      </div>
+
+      <div class="flex gap-2">
+        <span>Налог 5%:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>500 ₽</b>
+      </div>
+
+      <button
+          disabled=""
+          class="bg-lime-500 mt-4 w-full rounded-xl py-3 text-white hover:bg-lime-700 transition-all disabled:bg-slate-400 cursor-pointer">Оформить заказ</button>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "Drawer"
-}
-</script>
+
 
 <style scoped>
 
---
+++ src/components/HeadDrawer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center gap-5 mb-8">
+    <svg class="cursor-pointer opacity-30 hover:opacity-100 transition" width="35px" height="35px" viewBox="0 0 24 24"
+         fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 12H18M6 12L11 7M6 12L11 17" stroke="#000000" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round"/>
+    </svg>
+    <h2 class="text-2xl font-bold">Корзина</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
